8c54d993e736c2359fe64f7b671d28c3365bb608
Deivison Santos <deivisonddj3@gmail.com>
2025-12-28 19:44:44 -0300

    adding min time to search again

--- src/TypeRecognition.py
+++ src/TypeRecognition.py
@@ -4,13 +4,30 @@ import cv2
 import easyocr
 import queue
 import re
+import time
 
 class TypeRecognition:
-    def __init__(self, dataset_path, gpu=False):
+    def __init__(self, dataset_path, search_interval=5, gpu=False):
         self.lock_control = threading.Lock()
         self.dataset_path = dataset_path
         self.dataset = None
         self.reader = easyocr.Reader(["pt"], gpu=gpu)
+        self.search_interval = search_interval
+        self.last_found_search = None
+
+    def _can_search(self, time):
+        if self.search_interval is None:
+            return True
+        
+        if self.last_found_search is None:
+            return True
+        
+        elapsed_time = time - self.last_found_search
+        
+        if elapsed_time < self.search_interval:
+            return False
+
+        return True
 
     def _clean_text(self, text):
         return re.sub(r'[^a-zA-Z0-9\s]', '', text)
@@ -47,7 +64,10 @@ class TypeRecognition:
     def detect_text_in_frame(self, frame):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        if self.lock_control.acquire(blocking=False):
+        current_time = time.time()
+        can_search = self._can_search(current_time)
+
+        if can_search and self.lock_control.acquire(blocking=False):
             print("Procurando...")
             result_queue = queue.Queue()
 
@@ -56,6 +76,7 @@ class TypeRecognition:
             thread.join()
 
             if not result_queue.empty():
+                self.last_found_search = current_time
                 result = result_queue.get()
                 return result
         
