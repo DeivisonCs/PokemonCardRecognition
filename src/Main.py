6e2f245fcf5d3c64ca8833a9980b6e96ab7bd1c8
Deivison Santos <deivisonddj3@gmail.com>
2025-12-28 20:54:47 -0300

    adding image search option

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,12 @@ import cv2
 import pandas as pd
 import ObjectRecognition
 import TypeRecognition
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--img", help="Path to the pokemon image", default=None)
+args = parser.parse_args()
+img_path = args.img
 
 CAM_WIDTH, CAM_HEIGHT = 680, 480
 CAM_FPS = 60
@@ -16,14 +22,25 @@ def draw_last_results(frame, item):
         if item["type2"]:
             cv2.putText(frame, f"Type 2: {item["type2"]}", (10,83), cv2.FONT_ITALIC, .5, (0,255,255), 2)
 
-def main():
-    print("App started")
-    object_recognition = ObjectRecognition.ObjectRecognition()
-    object_recognition.load_model()
-
+def use_image(img_path):
+    frame = cv2.imread(img_path)
     type_recognition = TypeRecognition.TypeRecognition(dataset_path=DATASET_PATH)
     type_recognition.load_dataset()
+    
+    item_detected = type_recognition.detect_text_in_frame(frame)
+    
+    if item_detected is not None:
+        name = item_detected['name'].values[0]
+        type_1 = item_detected['type1'].values[0]
+        type_2 = item_detected['type2'].values[0] if pd.notnull(item_detected['type2'].values[0]) else None
+        type_recognition.last_item_found = {"name":name, "type1":type_1, "type2":type_2}
+        
+        print("Pokemon found")
+        print(f"Name: {name}")
+        print(f"Type 1: {type_1}")
+        print(f"Type 2: {type_2}")
 
+def use_cam():
     cap = cv2.VideoCapture(0)
     cap.set(3, CAM_WIDTH)
     cap.set(4, CAM_HEIGHT)
@@ -32,6 +49,12 @@ def main():
     if not cap.isOpened():
         print("Couldn't access camera")
         exit()
+
+    object_recognition = ObjectRecognition.ObjectRecognition()
+    object_recognition.load_model()
+
+    type_recognition = TypeRecognition.TypeRecognition(dataset_path=DATASET_PATH)
+    type_recognition.load_dataset()
 
     while True:
         success, frame = cap.read()
@@ -59,10 +82,20 @@ def main():
         cv2.imshow("App Cam", frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
-
+        
     cap.release()
     cv2.destroyAllWindows()
+
+def main(img_path):
+    print("App started")
+
+    if img_path:
+        use_image(img_path)
+    else:
+        use_cam()
+    
     print("App finished")
 
 if __name__ == "__main__":
-    main()
+    print(img_path)
+    main(img_path)
